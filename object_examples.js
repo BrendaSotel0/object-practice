9d1e3a145b88150284ad96fbdbf041001df924ca
Brenda Sotelo <neverthelessitstrue@yahoo.com>
2021-09-02 06:14:00 -0600

    Add car example

---
+++ object_examples.js
@@ -0,0 +1,9 @@
+var plymouthSpecialDeluxe = {
+year: 1947,
+color: 'black',
+vintage: true,
+isCool: true,
+isNotCool: false,
+doesNotHave: ['air conditioning', 'bluetooth', 'power steering', 'gps'],
+doesHave: ['suicide doors', 'spotlights', 'white wall tires'],
+}
